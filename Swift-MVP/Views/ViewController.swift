3aa6904fe5e3f1f8cd4a6804d8a6ee5043022917
Khoubaib Chamekh <khoubaib.chamekh@gmail.com>
2020-10-20 07:14:29 +0100

    #0000 init project

---
+++ Swift-MVP/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-MVP
+//
+//  Created by Khoubaib Chamekh on 10/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
